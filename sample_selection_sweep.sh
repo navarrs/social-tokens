664d2f21ab4f116509da1f937295f11387d6d79b
navarrs <ingrid.navarro.an@gmail.com>
2026-01-22 16:38:51 -0500

    Multiple file updates

--- sample_selection_sweep.sh
+++ sample_selection_sweep.sh
@@ -54,8 +54,8 @@ DEFAULT_MODELS=(
 )
 DEFAULT_DEVICES="0"
 DEFAULT_STRATEGIES=(
-  token_random_drop
   random_drop
+  token_random_drop
   simple_token_jaccard_drop
   simple_token_hamming_drop
   gumbel_token_jaccard_drop
