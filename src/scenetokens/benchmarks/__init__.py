87013a91eaa5591c7eb44eb60ac83f09b7ada9c1
navarrs <ingrid.navarro.an@gmail.com>
2026-04-09 19:17:13 -0400

    Refactored benchmark

--- src/scenetokens/benchmarks/__init__.py
+++ src/scenetokens/benchmarks/__init__.py
@@ -1,9 +1,11 @@
 from .causal_agents import create_causal_agents
+from .common import Benchmark
 from .ego_safeshift import create_ego_safeshift
 from .environments import create_environments
 
 
 __all__ = [
+    "Benchmark",
     "create_causal_agents",
     "create_ego_safeshift",
     "create_environments",
